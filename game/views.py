d388194380c5358db67e81a7bd42e6ed353a6ef3
Nina59 <devares3801@gmail.com>
2021-07-06 14:18:57 -0600

    Refactoring create view to fill model

--- game/views.py
+++ game/views.py
@@ -8,15 +8,23 @@ def Home(request):
 		game_id=random.randint(100000,999999)
 	return render(request, 'base.html', {"game_id" : game_id})
 
-def Create_game(request,player1,game_id):
-	game_id=int(game_id)
-	game=Game(player1=player1, player2= '' ,game_id=game_id)
-	game.save()
-	return render(request, 'create.html',{"game_id": game_id})
+def Create_game(request,player,game_id,fromwhere):
+	#If come from 'home' url create the game and fill player1 and game_id'
+	if fromwhere == "home":		
+		game_id = int(game_id)
+		game  = Game(player1=player, player2= '' ,game_id=game_id)
+		game.save()
+		#If come from 'join' url create the game and fill player1 and game_id'
+	if fromwhre == "join":
+		game  = Game.objects.filter(game_id=game_id)
+		game.player2=player
+		twoplayers= True
+	return render(request, 'create.html',{"game_id": game_id, "twoplayers": twoplayers})
 #Se debe guardar en db cuales se han usado para hacerlos invalidos
 # par nuevos usuarios
 
-def Join_game(request):
+def Join_game(request,player2):
+	
 	#buscar en video de crud como actualizar los registros
 	return render(request, 'join.html')
 
